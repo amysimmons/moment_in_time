592db46b5a9a183a1ef7a1c3f45433c1923f52ad
Amy Simmons <amy__simmons@hotmail.com>
2015-04-24 09:34:46 +1000

    instagram and twitter clases in lib folder
    fixed api keys

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "ET7bxzIUIYbb9iMBQgqQh9F29"
-  config.consumer_secret     = "FTRaPW75LjQyMHYQnujJlHuNZ2yDbQJxd3wPN5ZtLee1loy8ug"
-  config.access_token        = "42308418-i0npYmb8CYzD4ZJcrKNA0koyVUxPmmhnQ5Z0gB5LE"
-  config.access_token_secret = "wgHtMGWFu2M0RvVQWlidEqqpgrwLDlEEr3WttlJ1YacjS"
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
