620b11960dbfd95a23ae2c56b46a4ac74ed68869
Amy Simmons <amy__simmons@hotmail.com>
2015-06-08 17:02:09 +1000

    floating, breathing images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def random_floating_image_class
+    i = (1..10).to_a.sample
+    "floating-image-#{i}"
+  end
+
+  def random_page_position
+    top = rand(100..1000)
+    left = rand(1..1000)
+    "top: #{top}px; left: #{left}px;"
+  end
 end
